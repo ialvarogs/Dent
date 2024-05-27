4b7ca75d224933476631199e730e0d52c76ee354
Álvaro García <89483259+ialvarogs@users.noreply.github.com>
2024-05-27 13:39:00 -0400

    s

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ function AboutPage() {
     return (
         <div>
             <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illo eveniet vel reprehenderit aliquid reiciendis aliquam delectus at, quae quas saepe maiores atque cumque ea molestiae ipsam ipsa obcaecati doloribus nesciunt ullam accusamus nobis magni repudiandae repellendus. Asperiores veniam excepturi, voluptatem aspernatur nisi non in. Non architecto inventore rerum veritatis ad labore. In ab asperiores eius, incidunt dolore, nemo accusamus animi, odio eligendi et dicta hic nihil. Architecto qui eligendi nulla quo, eaque ipsam quibusdam, dolores voluptatibus numquam at ipsa earum delectus porro sequi sint soluta accusantium assumenda inventore quis suscipit enim molestias, autem totam! Eaque mollitia nemo neque consequatur.
+            <p>aLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illo eveniet vel reprehenderit aliquid reiciendis aliquam delectus at, quae quas saepe maiores atque cumque ea molestiae ipsam ipsa obcaecati doloribus nesciunt ullam accusamus nobis magni repudiandae repellendus. Asperiores veniam excepturi, voluptatem aspernatur nisi non in. Non architecto inventore rerum veritatis ad labore. In ab asperiores eius, incidunt dolore, nemo accusamus animi, odio eligendi et dicta hic nihil. Architecto qui eligendi nulla quo, eaque ipsam quibusdam, dolores voluptatibus numquam at ipsa earum delectus porro sequi sint soluta accusantium assumenda inventore quis suscipit enim molestias, autem totam! Eaque mollitia nemo neque consequatur.
 
             </p>
         </div>
